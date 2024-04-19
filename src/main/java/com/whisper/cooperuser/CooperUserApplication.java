2f3e62e51add99e56b2d6e96714b2d4213a2c5c3
marso34 <96871583+marso34@users.noreply.github.com>
2024-04-19 17:45:34 +0900

    init
    프로젝트 초기 설정

---
+++ src/main/java/com/whisper/cooperuser/CooperUserApplication.java
@@ -0,0 +1,13 @@
+package com.whisper.cooperuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whisper/cooperuser/CooperUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whisper.cooperuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
